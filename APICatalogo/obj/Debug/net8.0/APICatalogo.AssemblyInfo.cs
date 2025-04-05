3a990608c04cb801ec387ef0e9b22a6b0da469b9
Thiago <thiago.f.f.l@hotmail.co>
2025-04-05 09:45:31 -0400

    feat: Ajuste e otimizações - Serialização Json

--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eac857de36dc0de6f8cfff8ca95cf8960f8bd3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97d21017239ca3e0a0b51ce52f6f7a75091d707e")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
