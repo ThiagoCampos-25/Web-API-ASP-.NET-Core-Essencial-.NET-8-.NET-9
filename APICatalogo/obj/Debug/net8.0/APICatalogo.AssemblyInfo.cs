f91d6ba9cb3934f3324975b0f81c7523ad160d67
Thiago <thiago.f.f.l@hotmail.co>
2025-05-17 17:23:11 -0400

    feat:usando um middleware para realizar o tratamento de erros global

--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9479f2e45d725ef2aa1c5c44da72cd54849d59ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f18a93313d2ade437807326d3429bae1ca431c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
